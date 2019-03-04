4c33eb28c9a1367a6f4ef8194830c2b6b7864ec9
Diego <diego@hqo.co>
2019-03-03 21:09:55 -0500

    Remove broken imports for ViewStylePropTypes

--- src/components/PaymentCardTextField.js
+++ src/components/PaymentCardTextField.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
 import {
-  requireNativeComponent,
-  findNodeHandle,
-  StyleSheet,
-  View,
-  TouchableWithoutFeedback,
-  ViewPropTypes,
   Platform,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+  ViewPropTypes,
+  findNodeHandle,
+  requireNativeComponent,
 } from 'react-native'
+import React, { Component } from 'react'
+
 import PropTypes from 'prop-types'
-import StyleSheetPropType from 'react-native/Libraries/StyleSheet/StyleSheetPropType'
-import ViewStylePropTypes from 'react-native/Libraries/Components/View/ViewStylePropTypes'
 import TextInputState from 'react-native/Libraries/Components/TextInput/TextInputState'
 
 const FieldStylePropType = {
-  ...ViewStylePropTypes,
+  ...ViewPropTypes.style,
   color: PropTypes.string,
 }
 
@@ -38,7 +37,7 @@ const NativePaymentCardTextField = requireNativeComponent('TPSCardField', Paymen
 export default class PaymentCardTextField extends Component {
   static propTypes = {
     ...ViewPropTypes,
-    style: StyleSheetPropType(FieldStylePropType), // eslint-disable-line new-cap
+    style: FieldStylePropType,
 
     // Common
     expirationPlaceholder: PropTypes.string,
